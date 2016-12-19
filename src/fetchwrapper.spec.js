1e533be4ec5580025ab9fe45125f8752a0eedbf3
Andy Hill <andy@andyhill.us>
2016-12-18 21:13:51 -0500

    fix tests

--- src/fetchwrapper.spec.js
+++ src/fetchwrapper.spec.js
@@ -22,45 +22,49 @@ describe('fetchwrap', () =>  {
 			expect(fetchwrap.mocking).toBe(false);
 			fetchwrap.on();
 			expect(fetchwrap.mocking).toBe(true);
-
 		});
 	});
 
-
-	it('builds a structure of mocks', () => {
-		expect(fetchwrap.mocks).toEqual({});
-		standardMocks();
-        expect(fetchwrap.mocks).toEqual(standardMockStructure);
-        // console.log(JSON.stringify(fetchwrap.mocks));
+	describe('mock', () => {
+		it('builds a structure of mocks', () => {
+			expect(fetchwrap.mocks).toEqual({});
+			standardMocks();
+	        expect(fetchwrap.mocks).toEqual(standardMockStructure);
+	        // console.log(JSON.stringify(fetchwrap.mocks));
+		});
 	});
 
-	it('fixes the payload', () => {
-		standardMocks();
-		Object.getOwnPropertyNames(standardMockStructure).forEach(url => {
-			Object.getOwnPropertyNames(standardMockFixedStructure[url]).forEach(method => {
-				standardMockStructure[url][method].forEach((payload, index) => {
-					expect(fetchwrap.fixPayload(url, payload)).toEqual(standardMockFixedStructure[url][method][index]);
+	describe('fixPayload', () => {
+		it('fixes the payload', () => {
+			standardMocks();
+			Object.getOwnPropertyNames(standardMockStructure).forEach(url => {
+				Object.getOwnPropertyNames(standardMockFixedStructure[url]).forEach(method => {
+					standardMockStructure[url][method].forEach((payload, index) => {
+						expect(fetchwrap.fixPayload(url, payload)).toEqual(standardMockFixedStructure[url][method][index]);
+					});
 				});
 			});
 		});
 	});
 
-	it('mocks a mock', () => {
-		const payload = { foo: 'bar' };
-		fetchwrap.mock('GET', '/foo', payload);
-		fetch('/foo')
-			.then(response => response.json())
-			.then(response => expect(response).toEqual(payload))
-			.catch(error => console.error(error));
-	});
+	describe('fetch', () => {
+		it('mocks a mock', () => {
+			const payload = { foo: 'bar' };
+			fetchwrap.mock('GET', '/foo', payload);
+			fetch('/foo')
+				.then(response => response.json())
+				.then(response => expect(response).toEqual(payload))
+				.catch(error => console.error(error));
+		});
 
-	it('executes each matching mock in sequence and then repeats final match', () => {
-		standardMocks();
-		const firstMatchPayload = standardMockStructure['/foo']['GET'][0];
-		const secondMatchPayload = {};
-		fetch('/foo').then(response => expect(response).toEqual(firstMatchPayload));
-		fetch('/foo').then(response => expect(response).toEqual(secondMatchPayload));
-		fetch('/foo').then(response => expect(response).toEqual(secondMatchPayload));
+		it('executes each matching mock in sequence and then repeats final match', () => {
+			standardMocks();
+			const firstMatchPayload = standardMockStructure['/foo']['GET'][0];
+			const secondMatchPayload = {};
+			fetch('/foo').then(response => expect(response).toEqual(firstMatchPayload));
+			fetch('/foo').then(response => expect(response).toEqual(secondMatchPayload));
+			fetch('/foo').then(response => expect(response).toEqual(secondMatchPayload));
+		});
 	});
 
 	describe('urlPatternMatches', () => {
@@ -79,17 +83,19 @@ describe('fetchwrap', () =>  {
 		});		
 	});
 
-	it('should allow you to see which mocks were not called', () => {
-		standardMocks();
-		expect(fetchwrap.validate().length).toBe(4);
-		fetch('/foo');
-		expect(fetchwrap.validate().length).toBe(3);
-		fetch('/foo');
-		expect(fetchwrap.validate().length).toBe(2);
-		fetch('/foo', { method: 'POST' });
-		expect(fetchwrap.validate().length).toBe(1);
-		fetch('/bar');
-		expect(fetchwrap.validate().length).toBe(0);		
-		
+	describe('validate', () => {
+		it('should allow you to see which mocks were not called', () => {
+			standardMocks();
+			expect(fetchwrap.validate().length).toBe(4);
+			fetch('/foo');
+			expect(fetchwrap.validate().length).toBe(3);
+			fetch('/foo');
+			expect(fetchwrap.validate().length).toBe(2);
+			fetch('/foo', { method: 'POST' });
+			expect(fetchwrap.validate().length).toBe(1);
+			fetch('/bar');
+			expect(fetchwrap.validate().length).toBe(0);		
+			
+		});
 	});
 });
